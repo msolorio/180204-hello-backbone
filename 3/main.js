b87df0f50717abbbec09b9e29a8695c2c8325ab1
msolorio <msolorio789@gmail.com>
2018-02-04 22:55:35 -0800

    add example 3 with local storage

---
+++ 3/main.js
@@ -0,0 +1,84 @@
+var Item = Backbone.Model.extend({
+  defaults: {
+    text: 'hello',
+    color: 'pink'
+  }
+});
+
+var List = Backbone.Collection.extend({
+  model: Item
+});
+
+var ListView = Backbone.View.extend({
+  el: $('body'),
+
+  events: {
+    'click button#add': 'addItem'
+  },
+
+  initialize() {
+    var methods = [
+      'render',
+      'addItem',
+      'appendItem',
+      'updateFromPersistance',
+      'updatePersistanceOnAdd'
+    ];
+
+    _.bindAll(this, ...methods);
+
+    this.collection = new List();
+    this.collection.bind('add', this.appendItem);
+    this.counter = 0;
+
+    this.render();
+    this.updateFromPersistance();
+  },
+
+  render() {
+    $(this.el).append("<input id='inputText' type='text' placeholder='text'/>");
+    $(this.el).append("<input id='inputColor' type='text' placeholder='color'/>");
+    $(this.el).append("<button id='add'>add Item</button>");
+    $(this.el).append("<ul></ul>");
+  },
+
+  updateFromPersistance() {
+    if (!window.localStorage['hello-backbone-3-collection']) {
+      window.localStorage['hello-backbone-3-collection'] = JSON.stringify([]);
+    } else {
+      var storage = JSON.parse(window.localStorage['hello-backbone-3-collection']);
+      storage.forEach((item) => {
+        this.collection.add(item);
+      });
+    }
+  },
+
+  addItem() {
+    this.counter++;
+
+    var item = new Item();
+
+    item.set({
+      text: $('#inputText', this.el).val(),
+      color: $('#inputColor', this.el).val(),
+      number: this.counter
+    });
+
+    this.collection.add(item);
+    this.updatePersistanceOnAdd(item);
+  },
+
+  updatePersistanceOnAdd(item) {
+    var storage = JSON.parse(window.localStorage['hello-backbone-3-collection']);
+    storage.push(item);
+    window.localStorage['hello-backbone-3-collection'] = JSON.stringify(storage);
+  },
+
+  appendItem(item) {
+    $('ul', this.el).append(
+      `<li style="background: ${item.get('color')};">${item.get('number')}. ${item.get('text')}</li>`
+    );
+  }
+});
+
+var listView = new ListView();
